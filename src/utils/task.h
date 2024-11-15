2dda68a92ac4be568cd55c867ffdfd1e8e8c7c3f
guna-sd <sdgunaseker@gmail.com>
2024-11-16 03:19:09 +0530

    Task:: Enhancement of logic and code

--- src/utils/task.h
+++ src/utils/task.h
@@ -4,30 +4,49 @@
 #include <string>
 #include <vector>
 #include <ctime>
+#include <memory>
 #include "command.h"
 
-class Task 
+using namespace std;
+
+enum class TaskStatus { PENDING, PAUSED, COMPLETED, CANCELLED };
+enum class TaskPriority { Low, Medium, High };
+
+class Task
 {
+private:
+    std::string taskName;
+    TaskStatus status;
+    TaskPriority priority;
+    string description;
+    Command command;
+    bool isBackgroundTask;
 
 public:
-    Task(int id, const std::string& name, const std::string& recurrence, const std::vector<Command>& cmds)
-    : taskID(id), taskName(name), recurrence(recurrence), nextRun(0), lastRun(0), associatedCommands(cmds), 
-      isActive(true), taskStatus("pending"), owner(""), priority(0) {}
-    
-    
 
-private:
-    int taskID;
-    std::string taskName;
-    std::string recurrence;
-    std::time_t nextRun;
-    std::time_t lastRun;
-    std::vector<Command> associatedCommands;
-    bool isActive;                       // Indicates if the task is active
-    std::string taskStatus;              // Current status of the task (e.g., "pending", "completed", "failed")
-    std::string owner;                   // User or system that owns the task
-    int priority;
-}
+    Task(const string& name, const string& desc, const Command& cmd, bool isBg = false) 
+        : taskName(name), description(desc), command(cmd) , isBackgroundTask(isBg) {}
+
+    void execute()
+    {
+        if (!command.isExecuted()) {
+            command.execute();
+        } else {
+            cerr << "Command has already been executed." << endl;
+        }
+    }
+
+    bool isCompleted() const { return status == TaskStatus::COMPLETED || status == TaskStatus::CANCELLED; }
+    const string& getTaskName() const { return taskName; }
+
+    void printTaskInfo()
+    {
+        cout << "Task Name: " << taskName << endl;
+        cout << "Description: " << description << endl;
+        cout << "Command: " << command.toString() << endl;
+        cout << "Background Task: " << (isBackgroundTask? "Yes" : "No") << endl;
+    }
+};
 
 
 #endif
